fb4b66c24742d00893cc6a62dcd9d0adc00b6dc1
Slee Woo <mail@sleewoo.com>
2025-11-07 22:01:33 +0200

    package(react-generator): unit tests

---
+++ packages/generators/react-generator/test/path.test.ts
@@ -0,0 +1,46 @@
+import { describe, expect, test } from "vitest";
+
+import { pathTokensFactory } from "@kosmojs/devlib";
+
+import { pathFactory } from "../src/factory";
+
+describe("pathFactory", () => {
+  test("no params", () => {
+    expect(pathFactory(pathTokensFactory("some/page"))).toEqual("some/page");
+  });
+
+  test("no params with extension", () => {
+    expect(pathFactory(pathTokensFactory("some/page.html"))).toEqual(
+      "some/page.html",
+    );
+  });
+
+  test("required params", () => {
+    expect(pathFactory(pathTokensFactory("some/[param]"))).toEqual(
+      "some/:param",
+    );
+  });
+
+  test("optional params", () => {
+    expect(pathFactory(pathTokensFactory("some/[[param]]"))).toEqual(
+      "some/:param?",
+    );
+  });
+
+  test("rest params", () => {
+    expect(pathFactory(pathTokensFactory("some/[...param]"))).toEqual("some/*");
+  });
+
+  test("combined params", () => {
+    expect(
+      pathFactory(
+        pathTokensFactory("some/[required]/with/[[optional]]/and/[...rest]"),
+      ),
+    ).toEqual("some/:required/with/:optional?/and/*");
+  });
+
+  test("index prefix replaced with /", () => {
+    expect(pathFactory(pathTokensFactory("index"))).toEqual("");
+    expect(pathFactory(pathTokensFactory("index/[id]"))).toEqual(":id");
+  });
+});
